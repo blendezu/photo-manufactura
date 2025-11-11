e5df2e6922ea0b63da7345253231ddbcc613a8b2
Duong Tran <duongtran@nat079028.nat.FH-Koeln.DE>
2025-11-11 16:37:44 +0100

    corrected rotate for 16bit image

--- src/image_processing/operations/geometry/rotate.cpp
+++ src/image_processing/operations/geometry/rotate.cpp
@@ -1,3 +1,4 @@
+
 #include "rotate.h"
 
 #include <opencv2/core/hal/interface.h>
@@ -18,7 +19,7 @@ cv::Mat Rotate::apply(const cv::Mat& srcImg) {
         case CV_16UC1:  // 16 bit gray image
             return rotateImgTemplate<uint16_t>(srcImg, angle_deg, roi);
         case CV_16UC3:  // 8 bit RGB image
-            return rotateImgTemplate<cv::Vec3b>(srcImg, angle_deg, roi);
+            return rotateImgTemplate<cv::Vec3w>(srcImg, angle_deg, roi);
         default:
             throw std::invalid_argument("Rotate: unsupported image type\n");
     }
