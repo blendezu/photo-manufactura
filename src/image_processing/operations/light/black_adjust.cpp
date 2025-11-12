c1c6a0274e573dd6868881bab69df7514d48d57d
Duong Tran <duongtran@MacBookPro.fritz.box>
2025-11-12 21:28:50 +0100

    corrected adjust black for 16 bit

--- src/image_processing/operations/light/black_adjust.cpp
+++ src/image_processing/operations/light/black_adjust.cpp
@@ -2,6 +2,7 @@
 
 #include <opencv2/core/hal/interface.h>
 
+#include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 
 cv::Mat AdjustBlack::apply(const cv::Mat& srcImg) {
@@ -9,5 +10,19 @@ cv::Mat AdjustBlack::apply(const cv::Mat& srcImg) {
         std::cerr << "Error in AdjustBlack: empty input image\n";
         return cv::Mat();
     }
-    return blackGrayImgTemplate<uchar>(srcImg, black / 800.0f);
+
+    float blackFactor = black / 800.0f;
+    switch (srcImg.type()) {
+        case CV_8UC1:
+            return blackGrayImgTemplate<uchar>(srcImg, blackFactor);
+        case CV_16UC1:
+            return blackGrayImgTemplate<ushort>(srcImg, blackFactor);
+        case CV_8UC3:
+            return blackRGBImgTemplate<cv::Vec3b>(srcImg, blackFactor);
+        case CV_16UC3:
+            return blackRGBImgTemplate<cv::Vec3w>(srcImg, blackFactor);
+        default:
+            std::cerr << "Error in AdjustBlack: unsupported image type\n";
+            return cv::Mat();
+    }
 }
