29c9e54a598ea22a47a298d1bfb61f03a23a7ed6
Duong Tran <duongtran@Macbook-Fake.fritz.box>
2025-11-25 22:19:55 +0100

    add loadImg and saveImg to Document

--- src/document/document.h
+++ src/document/document.h
@@ -3,11 +3,17 @@
 
 #include <QImage>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 class Document {
    public:
-    cv::Mat Q2Mat(const QImage& img);
-    QImage Mat2Q(const cv::Mat& mat);
+    static cv::Mat Q2Mat(const QImage& img);
+
+    static QImage Mat2Q(const cv::Mat& mat);
+
+    static cv::Mat loadImg(std::string path);
+
+    static void saveImg(const cv::Mat& img, std::string fileName);
 };
 
 #endif
