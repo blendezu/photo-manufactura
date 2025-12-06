52f440f0812db17c4491a0fc88dfb9e870b5d65f
Duong Tran <duongtran@MacBookPro.fritz.box>
2025-12-06 22:28:48 +0100

    add AdjustWhite to fused pipeline

--- src/image_processing/operations/light/white_adjust.cpp
+++ src/image_processing/operations/light/white_adjust.cpp
@@ -1,52 +1,187 @@
 #include "white_adjust.h"
 
+#include <Halide.h>
 #include <opencv2/core/hal/interface.h>
 
-#include <opencv2/core/mat.hpp>
+#include <iostream>
+#include <opencv2/imgproc.hpp>
+#include <vector>
 
 #include "color_space.h"
+#include "halide_color_space.h"
+#include "halide_image_utils.h"
 #include "image_utils.h"
 
+// ===================================================================
+// 1. Prepare Parameters on CPU for GPU
+// ===================================================================
+
+void AdjustWhite::prepareParameters(const cv::Mat& srcImg) {
+    if (srcImg.empty()) {
+        std::cerr << "[AdjustWhite] Error: The input image is empty\n";
+        return;
+    }
+
+    // --- A. Find the Min & Max Value (Statistic) ---
+
+    // Find min/max in a 512x 512 thumbnail image for better performance
+    cv::Mat thumbnail = ImageUtils::createThumbnail(srcImg);
+
+    // Since 'requiresFreshStats' is true, srcImg contains the result of all previous operations.
+    if (srcImg.channels() == 3) {
+        cv::Mat hslThumbnail = ColorSpace::convertBGR2HSL(thumbnail);
+        auto minMax = ImageUtils::calculateMinMax(hslThumbnail, 2);  // channel 2 is Luminance
+        m_minL = std::get<0>(minMax);
+        m_maxL = std::get<1>(minMax);
+    }
+    // If gray image
+    else {
+        auto minMax = ImageUtils::calculateMinMax(thumbnail, 0);
+        m_minL = std::get<0>(minMax);
+        m_maxL = std::get<1>(minMax);
+    }
+
+    // --- B. Calculate Logic Parameters ---
+    float range = m_maxL - m_minL;
+
+    // Determine the dynamic threshold based on the image's actual dynamic range
+    float underVal = m_minL + (range * WEIGHT_RANGE_LOWER);
+    float upperVal = m_minL + (range * WEIGHT_RANGE_UPPER);
+
+    float factor = static_cast<float>(m_white) / WHITE_SCALING_FACTOR;
+    float maxRange = (srcImg.depth() == CV_8U) ? 255.0f : 65535.0f;
+
+    // --- C. Update Halide Runtime Parameters ---
+    p_underVal.set(underVal);
+    p_upperVal.set(upperVal);
+    p_whiteFactor.set(factor);
+    p_maxRange.set(maxRange);
+
+    std::cout << "[DEBUG] AdjustWhite::prepareParameters() called" << std::endl;
+    std::cout << "[DEBUG] m_white = " << m_white << std::endl;
+    std::cout << "[DEBUG] Setting p_whiteFactor = " << factor << std::endl;
+}
+
+// ===================================================================
+// 2. Build Graph (Halide - GPU)
+// ===================================================================
+
+Halide::Func AdjustWhite::buildGraph(Halide::Func srcImg, Halide::Var x, Halide::Var y,
+                                     Halide::Var c) {
+    std::cout << "[DEBUG] AdjustWhite::buildGraph() called" << std::endl;
+    std::cout << "[DEBUG] p_whiteFactor value = " << p_whiteFactor.get() << std::endl;
+    std::cout << "[DEBUG] p_underVal = " << p_underVal.get() << std::endl;
+    std::cout << "[DEBUG] p_upperVal = " << p_upperVal.get() << std::endl;
+    // Inversed max range to avoid Division later
+    Halide::Expr invMaxRange = 1.0f / p_maxRange;
+
+    // --- Path A. Gray image ---
+    if (srcImg.dimensions() == 2) {  // Gray image has only 2 dimensions (x, y)
+        Halide::Expr currVal = srcImg(x, y) * invMaxRange;
+        Halide::Expr weight =
+            HalideImageUtils::calculateBrightWeight(currVal, p_underVal, p_upperVal);
+        Halide::Expr deltaVal = weight * p_whiteFactor;
+        Halide::Expr newVal = Halide::clamp(currVal + deltaVal, 0.0f, 1.0f);
+
+        Halide::Func dstImg("adjust_white_gray_image");
+
+        // Convert back to original Bit Depth
+
+        std::cout << "[DEBUG] AdjustWhite::buildGraph() Gray image" << std::endl;
+        std::cout << "[DEBUG] AdjustWhite::buildGraph() called" << std::endl;
+        std::cout << "[DEBUG] Current p_whiteFactor = " << p_whiteFactor.get() << std::endl;
+        dstImg(x, y) = newVal * p_maxRange;
+        return dstImg;
+    }
+    std::cout << "[DEBUG] AdjustWhite::buildGraph() color image" << std::endl;
+    std::cout << "[DEBUG] AdjustWhite::buildGraph() called" << std::endl;
+    std::cout << "[DEBUG] Current p_whiteFactor = " << p_whiteFactor.get() << std::endl;
+
+    // --- Path B. Color Image ---
+
+    // 1. Channel Splitting & HSL Conversion
+    Halide::Expr B = srcImg(x, y, 0) * invMaxRange;
+    Halide::Expr G = srcImg(x, y, 1) * invMaxRange;
+    Halide::Expr R = srcImg(x, y, 2) * invMaxRange;
+
+    std::vector<Halide::Expr> hslImg = HalideColorSpace::BGR2HSL(B, G, R);
+    Halide::Expr currL = hslImg[2];  // Current Luminance
+
+    // 2. Weight Calculation
+    Halide::Expr weight = HalideImageUtils::calculateBrightWeight(currL, p_underVal, p_upperVal);
+
+    // 3. Apply Adjustment
+    Halide::Expr deltaL = weight * p_whiteFactor;
+    Halide::Expr newL = Halide::clamp(currL + deltaL, 0.0f, 1.0f);
+
+    // 4. Convert back to BGR
+    std::vector<Halide::Expr> bgr = HalideColorSpace::HSL2BGR(hslImg[0], hslImg[1], newL);
+
+    // 5. Channel Selection (Re-interleave)
+    Halide::Expr outColor = Halide::select(c == 0, bgr[0], Halide::select(c == 1, bgr[1], bgr[2]));
+
+    Halide::Func dstImg("adjust_white_image");  // output image
+
+    // 6. Denormalize to original bit depth image
+    dstImg(x, y, c) = outColor * p_maxRange;
+    return dstImg;
+}
+
+// ===================================================================
+// 3. CPU Implementation
+// ===================================================================
 cv::Mat AdjustWhite::apply(const cv::Mat& srcImg) {
     if (srcImg.empty()) {
-        std::cerr << "Error in AdjustWhite: empty image\n";
+        std::cerr << "[AdjustWhite] Error: Input image is empty\n";
         return cv::Mat();
     }
 
-    float whiteFactor = white / 800.0f;
+    // 1. Prepare Parameters
+    // Normalize user input (-100 -> 100) to internal factor
+    float changeFactor = static_cast<float>(m_white) / WHITE_SCALING_FACTOR;
 
+    // --- A. Color Images 8-/16-bit ---
     if (srcImg.type() == CV_8UC3 || srcImg.type() == CV_16UC3) {
-        cv::Mat hslImg = ColorSpace::convertBGR2HSL(srcImg);
+        // 1. Statistics Calculation on 512x512 thumbnail image for better performance
+        cv::Mat thumbnail = ImageUtils::createThumbnail(srcImg);
+        cv::Mat hslThumbnail = ColorSpace::convertBGR2HSL(thumbnail);
 
-        auto start = std::chrono::high_resolution_clock::now();
-
-        auto minMaxVal = ImageUtils::calculateMinMax(hslImg, 2);
+        auto minMaxVal = ImageUtils::calculateMinMax(hslThumbnail, 2);
         float minL = std::get<0>(minMaxVal);
         float maxL = std::get<1>(minMaxVal);
 
-        auto weightParams = ImageUtils::precalculateWhiteWeightParams(minL, maxL, 0.7, 0.9);
+        // 2. Pre-calculate weighting logic
+        auto weightParams = ImageUtils::precalculateWhiteWeightParams(
+            minL, maxL, WEIGHT_RANGE_LOWER, WEIGHT_RANGE_UPPER);
 
-#pragma omp parallel for
+        // 3. Convert to HSL
+        cv::Mat hslImg = ColorSpace::convertBGR2HSL(srcImg);
+        float testL = hslImg.at<cv::Vec3f>(100, 100)[2];  // Luminance
+        std::cout << "[DEBUG] CPU HSL-L at (100,100): " << testL << std::endl;
+
+// 4. Parallel Pixel Processing
+// clang-format off
+        #pragma omp parallel for
+        // clang-format on
         for (int y = 0; y < hslImg.rows; y++) {
-            float* __restrict hslPtr = hslImg.ptr<float>(y);
+            float* hslPtr = hslImg.ptr<float>(y);
 
-            int len = hslImg.cols * 3;
+            int len = hslImg.cols * 3;  // Dimension for 1D-Arry
 
-            for (int x = 2; x < len; x += 3) {
+            for (int x = 2; x < len; x += 3) {  // Start at 2 and increment 3 because H S L H S L
                 float currL = hslPtr[x];
 
                 float weight = ImageUtils::calculateBrightWeight(currL, weightParams);
 
-                float whiteChange = weight * whiteFactor;
-                float newL = std::clamp(currL + whiteChange, 0.0f, 1.0f);
+                float deltaL = weight * changeFactor;
+                float newL = std::clamp(currL + deltaL, 0.0f, 1.0f);
 
                 hslPtr[x] = newL;
             }
         }
+
+        // 5. Convert back to BGR
         if (srcImg.depth() == CV_8U) {
-            auto end = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-            std::cout << duration.count() << std::endl;
             return ColorSpace::convertHSL2BGR(hslImg, 8);
         } else {
             return ColorSpace::convertHSL2BGR(hslImg, 16);
@@ -54,14 +189,15 @@ cv::Mat AdjustWhite::apply(const cv::Mat& srcImg) {
     }
 
     else if (srcImg.type() == CV_8UC1) {
-        return whiteGrayImgTemplate<uchar>(srcImg, whiteFactor);
+        return whiteGrayImgTemplate<uint8_t>(srcImg, changeFactor);
     }
 
     else if (srcImg.type() == CV_16UC1) {
-        return whiteGrayImgTemplate<ushort>(srcImg, whiteFactor);
+        return whiteGrayImgTemplate<uint16_t>(srcImg, changeFactor);
     }
 
     else {
+        std::cerr << "[AdjustWhite] Error: Unsupported image type\n";
         return cv::Mat();
     }
 }
