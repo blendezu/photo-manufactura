ceffea67e1c3ea508a3f025c5a9265e04a9ce06e
Duong Tran <duongsemail97@gmail.com>
2025-12-10 20:41:45 +0100

    upgrade AdjustContrast to GPU Fusion
    Former-commit-id: 59ad1682400a5da12863bee23b34511205aeb585

--- src/image_processing/operations/light/contrast_adjust.h
+++ src/image_processing/operations/light/contrast_adjust.h
@@ -1,13 +1,33 @@
+#include <Halide.h>
+
 #include <string>
 
 #include "operation_base.h"
 
-class AdjustContrast : public ImageOperation {
+class AdjustContrast : public HalideOperation {
    private:
-    int contrast;
+    int m_contrast;
+
+    // --- Halide Runtime Parameters ---
+    Halide::Param<float> p_maxRange{"maxRange"};
+    Halide::Param<float> p_contrastFactor{"contrastFactor"};
+
+    // --- Constant Parameters ---
+    static constexpr float CONTRAST_SCALING_FACTOR = 100.0f;
 
    public:
-    AdjustContrast(int value) : contrast(value) {}
+    AdjustContrast(int value) : m_contrast(value) {
+        p_maxRange.set(255.0f);
+    }
+
+    bool supportsHalide() const override {
+        return true;
+    }
+
+    void prepareParameters(const cv::Mat& srcImg) override;
+
+    Halide::Func buildGraph(Halide::Func srcImg, Halide::Var x, Halide::Var y,
+                            Halide::Var c) override;
 
     cv::Mat apply(const cv::Mat& srcImg) override;
 
@@ -16,14 +36,45 @@ class AdjustContrast : public ImageOperation {
     }
 
     std::string getSettings() const override {
-        return "contrast: " + std::to_string(contrast);
+        return "contrast: " + std::to_string(m_contrast);
     }
 
     void setContrast(int value) {
-        contrast = std::clamp(value, -100, 100);
+        m_contrast = std::clamp(value, -100, 100);
     }
 
     int getContrast() {
-        return contrast;
+        return m_contrast;
+    }
+
+   private:
+    template <typename T>
+    cv::Mat contrastGrayImgTemplate(const cv::Mat& srcImg, float contrastFactor) {
+        // 1. Create Destination Image
+        cv::Mat dstImg(srcImg.size(), srcImg.type());
+
+        // 2. Define the half max Range for calculation later
+        int halfMaxRange = (srcImg.depth() == CV_8U) ? 128 : 32768;
+
+        // clang-format off
+        // 3. Iteration through the image
+        #pragma omp parallel for
+        // clang-format on
+
+        for (int y = 0; y < srcImg.rows; y++) {
+            // 3.1 Get the pointers using __restrict to tell compiller that the pointer are not
+            // aliased.
+            const T* __restrict srcPtr = srcImg.ptr<T>(y);
+            T* __restrict dstPtr = dstImg.ptr<T>(y);
+
+            // 3.2 Calculate pixel-wise
+            for (int x = 0; x < srcImg.cols; x++) {
+                // 3.2.1 Calculate the new value
+                int newVal =
+                    static_cast<int>((srcPtr[x] - halfMaxRange) * contrastFactor + halfMaxRange);
+                dstPtr[x] = cv::saturate_cast<T>(newVal);
+            }
+        }
+        return dstImg;
     }
 };
