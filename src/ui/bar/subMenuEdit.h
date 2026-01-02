f257944d6c46816beb0992b7c8aa5a72e2acad30
ghrio <ghiri@gmx.de>
2026-01-02 04:47:57 +0100

    feat: Add image transformation features including rotate, flip, and crop operations
    Former-commit-id: 32c7fca3d808d4bb16d4b213a94b38b272100077

--- src/ui/bar/subMenuEdit.h
+++ src/ui/bar/subMenuEdit.h
@@ -10,12 +10,26 @@ class SubMenuEdit : public QMenu {
     explicit SubMenuEdit(QWidget* parent = nullptr);
     // Destructor
     ~SubMenuEdit();
-    // Slots for actions
-   private slots:
-    void onUndoTriggered();
-    void onRedoTriggered();
-    // Actions :
+
+   Q_SIGNALS:
+    // Edit signals
+    void undoRequested();
+    void redoRequested();
+
+    // Geometry signals
+    void rotateLeftRequested();   // -90 degrees
+    void rotateRightRequested();  // +90 degrees
+    void flipHorizontalRequested();
+    void flipVerticalRequested();
+
    private:
+    // Undo/Redo actions
     QAction* undoAction;
     QAction* redoAction;
+
+    // Geometry actions
+    QAction* rotateLeftAction;
+    QAction* rotateRightAction;
+    QAction* flipHorizontalAction;
+    QAction* flipVerticalAction;
 };
